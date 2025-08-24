4870ac2dd098aacd04b67774e2331f5695ad779f
Abhishekkumar2007 <kumarabhishek81457@gmail.com>
2025-08-24 17:56:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-print("hello world")
-    app.run(host='0.0.0.0', port=port)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Flask API is running on Railway!"
+
+if __name__ == '_main_':
+    app.run(host='0.0.0.0', port=5000)
+
 
