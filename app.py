7eae8f1c1cfedffda7c09dc0afadc9ef34d3c6b6
Abhishekkumar2007 <kumarabhishek81457@gmail.com>
2025-08-24 17:45:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route('/mydata', methods=['GET'])
-def my_data():
-    data = [
-        {"name": "Abhishek Kumar", "email": "abhishek@example.com"},
-        {"name": "Rahul Singh", "email": "rahul@example.com"}
-    ]
-    return jsonify(data)
-
-if __name__ == '_main_':
-
+print("hello world")
     app.run(host='0.0.0.0', port=port)
+
